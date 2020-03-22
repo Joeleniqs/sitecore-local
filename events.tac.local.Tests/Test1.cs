0ae3692307c476944f18a39cc09975adf3d94d90
Enikanorogbon Joel <joeleniqs@gmail.com>
2020-03-22 15:14:35 -0400

    first commit

---
+++ events.tac.local.Tests/Test1.cs
@@ -0,0 +1,14 @@
+﻿using Xunit;
+
+namespace events.tac.local.Tests
+{
+    public class Test1
+    {
+        [Fact]
+        public void FailedTest() 
+        {
+            Assert.True(true) ;
+        }
+    }
+}
+    
